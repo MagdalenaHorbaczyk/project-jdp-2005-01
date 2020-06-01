228c04ee6bf78ebd1a9716212d46c0a1fd892761
Mateusz <mat-wit100@wp.pl>
2020-06-01 22:17:34 +0200

    JDP200501-3	CartController - wstępna implementacja endpointów

--- src/main/java/com/kodilla/ecommercee/CartController.java
+++ src/main/java/com/kodilla/ecommercee/CartController.java
@@ -1,4 +1,36 @@
 package com.kodilla.ecommercee;
 
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/v1/cart")
+
 public class CartController {
+
+
+    @RequestMapping(method = RequestMethod.POST,value = "createCart")
+    public void createCart(@RequestBody CartDto cartDto) {
+        System.out.println("Sztuczne dane - post");
+    }
+
+    @RequestMapping(method = RequestMethod.GET,value = "getCartProducts")
+    public List<String> getCartProducts(@RequestParam Long cartId) {
+    return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,value = "addProductToCart")
+    public void addProductToCart(@RequestParam Long cartId, @RequestParam Long productId) {
+        System.out.println("Sztuczne dane - put");
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE,value = "removeProductFromCart")
+    public void removeProductFromCart(@RequestParam Long cartId, @RequestParam Long productId) {
+        System.out.println("Sztuczne dane - delete");
+    }
 }
---
+++ src/main/java/com/kodilla/ecommercee/CartDto.java
@@ -0,0 +1,18 @@
+package com.kodilla.ecommercee;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartDto {
+    private Long id;
+    private List<String> products = new ArrayList<>();
+}
