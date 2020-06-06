e483e0b2dade3c3f7f29e995b669da0fc24f5f9c
Mateusz Ryndak <kontakt@mryndak.pl>
2020-06-06 09:43:32 +0200

    Create user

---
+++ src/main/java/com/kodilla/ecommercee/entity/User.java
@@ -0,0 +1,26 @@
+package com.kodilla.ecommercee.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String name;
+}
