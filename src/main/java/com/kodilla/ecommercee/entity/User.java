bf5bfb6fd6f963b5f9b6798d9e1e32075eca05d7
Mateusz Ryndak <kontakt@mryndak.pl>
2020-06-06 09:57:19 +0200

    Create user - update v2

--- src/main/java/com/kodilla/ecommercee/entity/User.java
+++ src/main/java/com/kodilla/ecommercee/entity/User.java
@@ -5,10 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @NoArgsConstructor
 @AllArgsConstructor
