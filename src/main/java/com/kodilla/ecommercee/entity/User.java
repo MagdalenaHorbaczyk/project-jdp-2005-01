fe5a16ee2408d82468befa87a80ebaf037bf7672
Mateusz Ryndak <kontakt@mryndak.pl>
2020-06-06 09:36:10 +0200

    Create User entity

---
+++ src/main/java/com/kodilla/ecommercee/entity/User.java
@@ -0,0 +1,26 @@
+package com.kodilla.ecommercee.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Entity
+@Table
+public class User {
+
+    @Id
+    @NotNull
+    @GeneratedValue
+    private Long id;
+
+    @Column("USER_NAME")
+    private String login;
+
+    private String userKey;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+}
