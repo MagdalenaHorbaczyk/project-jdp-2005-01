1c48fc9e0f2f1c0b5cb380a1395b9d32d9891857
martyna30 <prawdzikmartyna@op.pl>
2020-06-10 15:38:30 +0200

    JDP200501-20

---
+++ src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.kodilla.ecommercee.repository;
+
+import com.kodilla.ecommercee.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public interface UserRepository extends CrudRepository<User, Long> {
+}
