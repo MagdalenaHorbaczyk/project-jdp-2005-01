73d3cababa185040a8390b9e823a3dc2ddf81b43
Mateusz <mat-wit100@wp.pl>
2020-06-15 22:13:22 +0200

    JDP200501-3	CartController - wstępna implementacja endpointów - poprawa komentarzy, Rev_03 implementacja listy <ProductDto>

--- src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -5,8 +5,23 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 @Transactional
 public interface GroupRepository extends CrudRepository<Group, Long> {
+
+    @Override
+    Group save(Group group);
+
+    void deleteById(Long id);
+
+    @Override
+    List<Group> findAll();
+
+    @Override
+    Optional<Group> findById(final Long id);
+
+
 }
