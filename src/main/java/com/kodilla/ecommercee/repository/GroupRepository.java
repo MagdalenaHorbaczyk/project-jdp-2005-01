dabfa76ccaff98f59dabe9401a48df1e231280e1
Mateusz <mat-wit100@wp.pl>
2020-06-15 22:28:02 +0200

    Zadanie JDP200501-18 Testy endpoint'ów - Group Rev_01

--- src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -1,27 +1,24 @@
 package com.kodilla.ecommercee.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.kodilla.ecommercee.entity.Group;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
-import java.util.List;
-import java.util.Optional;
-
 @Repository
-@Transactional
 public interface GroupRepository extends CrudRepository<Group, Long> {
 
     @Override
     Group save(Group group);
 
-    void deleteById(Long id);
+    void deleteById(Long Id);
 
     @Override
     List<Group> findAll();
 
     @Override
-    Optional<Group> findById(final Long id);
-
+    Optional<Group> findById(final Long Id);
 
 }
