70f630d2c655763211b623d10762a747eec0080e
darosg1 <darosg1@yahoo.com>
2020-06-20 15:11:58 +0200

    JDP200501-25

--- src/main/java/com/kodilla/ecommercee/service/UserDbService.java
+++ src/main/java/com/kodilla/ecommercee/service/UserDbService.java
@@ -1,4 +1,36 @@
 package com.kodilla.ecommercee.service;
 
+import com.kodilla.ecommercee.entity.User;
+import com.kodilla.ecommercee.exception.UserNotFoundException;
+import com.kodilla.ecommercee.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserDbService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User saveUser(final User user){
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUser(final Long userId){
+        return userRepository.findById(userId);
+    }
+
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
+    public boolean isExist(Long userId){
+        return userRepository.existsById(userId);
+    }
+
 }
