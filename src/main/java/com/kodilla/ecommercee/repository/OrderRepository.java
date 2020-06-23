30d9e3405a21dedf2eb311193ac72a0a25752938
martyna30 <prawdzikmartyna@op.pl>
2020-06-23 11:53:34 +0200

    JDP200501-24

--- src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
@@ -1,13 +1,4 @@
 package com.kodilla.ecommercee.repository;
 
-import com.kodilla.ecommercee.entity.Order;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import javax.transaction.Transactional;
-
-@Repository
-@Transactional
-public interface OrderRepository extends CrudRepository<Order, Long> {
-
+public interface OrderRepository {
 }
